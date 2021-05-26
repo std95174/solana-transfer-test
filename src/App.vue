5006b15795e13c1373a238f951397bd72a2ca351
Frank Wang <frankwang95174@gmail.com>
2021-05-26 21:48:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,95 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import {
+  Account,
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  TransactionInstruction,
+  Transaction,
+  sendAndConfirmTransaction
+} from "@solana/web3.js";
+import { Token } from "@solana/spl-token";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    //
+  }),
+  methods: {
+    async establishConnection() {
+      const rpcUrl = "https://devnet.solana.com";
+      let connection = new Connection(rpcUrl, "confirmed");
+      const version = await connection.getVersion();
+      console.log("Connection to cluster established:", rpcUrl, version);
+    }
+  },
+  async mounted() {
+    setTimeout(async () => {
+      await window.solana.connect();
+      const provider = window.solana;
+      console.log(provider.publicKey.toString());
+      const connection = new Connection("https://devnet.solana.com");
+      const { blockhash } = await connection.getRecentBlockhash();
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+      let transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: provider.publicKey,
+          toPubkey: provider.publicKey,
+          lamports: 100000000
+        })
+      );
+      transaction.feePayer = provider.publicKey;
+      transaction.recentBlockhash = blockhash;
+
+      let signed = await provider.signTransaction(transaction);
+      let signature = await connection.sendRawTransaction(signed.serialize());
+      await connection.confirmTransaction(signature);
+      console.log("received!");
+    }, 3000);
+  },
+  async created() {}
+};
+</script>
